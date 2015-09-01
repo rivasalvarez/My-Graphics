51be038c8b04864e938eda14960e6074b366c28b
Marco Rivas <mrivasalvarez@nevada.unr.edu>
2015-09-01 15:49:54 -0700

    first

---
+++ PA1/Matrix-Example-master/src/Shader.cpp
@@ -0,0 +1,26 @@
+#ifndef SHADER_CPP
+#define SHADER_CPP
+#include "Shader.h"
+#include <fstream>
+#include <streambuf>
+
+Shader::Shader(){
+}
+
+Shader::~Shader(){
+}
+
+string Shader::Load(const string filename) {
+ string source;
+ ifstream fin(filename.c_str(),ios::in);
+ string line = "";
+ while(!fin.eof()){
+   getline(fin,line);
+   source.append(line + '\n');
+ }
+ fin.close();
+return source;
+}
+
+#endif
+
---
+++ PA1/Matrix-Example-master/src/Shader.h
@@ -0,0 +1,11 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+class Shader{
+public:
+       Shader();
+       ~Shader();
+       string Load(const string);
+private:
+};
+#endif
